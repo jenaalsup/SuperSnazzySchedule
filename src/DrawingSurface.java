46061ce908029c90ffc0b95b8704ebbe1f7977d9
jenaalsup <63277580+jenaalsup@users.noreply.github.com>
2020-04-21 22:54:53 -0700

    Main method + some DrawingSurface
    Completed the main method as well as added a draw method to DrawingSurface.

--- src/DrawingSurface.java
+++ src/DrawingSurface.java
@@ -1,4 +1,14 @@
+import processing.core.PApplet;
 
-public class DrawingSurface {
-
+public class DrawingSurface extends PApplet
+{
+	public DrawingSurface()
+	{
+		
+	}
+	
+	public void draw()
+	{
+		this.rect(20, 20, width - 400, height - 40, 40);
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
 
-public class Main {
+public class Main 
+{
+	public static void main(String args[]) 
+	{
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
 
+		window.setSize(1250, 700);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);	
+		canvas.requestFocus();
+	}	
 }
+
